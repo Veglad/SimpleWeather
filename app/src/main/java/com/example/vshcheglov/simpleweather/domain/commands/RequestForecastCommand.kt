7a532ab8bf5f7d91601803065fd6ea96797ba30e
Vladyslav <rek0ra921@gmail.com>
2019-01-22 13:52:08 +0200

    weatherRequestFeature > Added RequstForecastCommand class with execute function that takes city zip code and returns ForecastList

---
+++ app/src/main/java/com/example/vshcheglov/simpleweather/domain/commands/RequestForecastCommand.kt
@@ -0,0 +1,15 @@
+package com.example.vshcheglov.simpleweather.domain.commands
+
+import com.example.vshcheglov.simpleweather.data.ForecastRequest
+import com.example.vshcheglov.simpleweather.domain.model.ForecastList
+import com.example.vshcheglov.simpleweather.mappers.ForecastDataMapper
+
+class RequestForecastCommand(val zipCode: String) :
+        Command<ForecastList> {
+    override fun execute(): ForecastList {
+        val forecastRequest = ForecastRequest(zipCode)
+        return  ForecastDataMapper().convertFromDataModel(
+            forecastRequest.execute()
+        )
+    }
+}
