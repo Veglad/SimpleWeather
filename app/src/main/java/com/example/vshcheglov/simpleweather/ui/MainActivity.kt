1778460be8b2efba639a12b5beb60b044f7738e3
Vladyslav <rek0ra921@gmail.com>
2019-01-22 15:57:55 +0200

    weatherRequestFeature > Temporary implementation of the MainActivity for debugging app. Init adapter and fill it with the data from the request result

--- app/src/main/java/com/example/vshcheglov/simpleweather/ui/MainActivity.kt
+++ app/src/main/java/com/example/vshcheglov/simpleweather/ui/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.vshcheglov.simpleweather.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.example.vshcheglov.simpleweather.R
+import com.example.vshcheglov.simpleweather.domain.commands.RequestForecastCommand
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+
+        doAsync {
+            val result = RequestForecastCommand("94043").execute()
+            uiThread {
+                forecastList.adapter = ForecastListAdapter(result)
+            }
+        }
     }
 }
