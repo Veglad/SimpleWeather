74362e234a5ca1eba81e31b67c0491d3b90a2130
Vladyslav <rek0ra921@gmail.com>
2019-01-23 13:27:15 +0200

    weather_request > Correct views id in the item_forecast layout

--- app/src/main/java/com/example/vshcheglov/simpleweather/ui/ForecastListAdapter.kt
+++ app/src/main/java/com/example/vshcheglov/simpleweather/ui/ForecastListAdapter.kt
@@ -4,14 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.example.vshcheglov.simpleweather.R
 import com.example.vshcheglov.simpleweather.domain.model.Forecast
 import com.example.vshcheglov.simpleweather.domain.model.ForecastList
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_forecast.view.*
-import org.jetbrains.anko.find
 
 class ForecastListAdapter(private val weekForecast: ForecastList,
                           private val itemClick: (Forecast) -> Unit) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
@@ -36,10 +33,10 @@ class ForecastListAdapter(private val weekForecast: ForecastList,
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
                 Picasso.get().load(iconUrl).into(view.itemForecastIcon)
-                view.itemForecastDateTextView.text = date
+                view.dateTextView.text = date
                 view.descriptionTextView.text = description
-                view.itemForecastMaxTemperatureTextView.text = high.toString()
-                view.itemForecastMinTemperatureTextView.text = low.toString()
+                view.maxTemperatureTextView.text = high.toString()
+                view.minTemperatureTextView.text = low.toString()
                 itemView.setOnClickListener{itemClick(forecast)}
             }
         }
