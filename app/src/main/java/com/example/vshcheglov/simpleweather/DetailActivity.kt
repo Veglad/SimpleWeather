c90e74e81d637e931b1a7e03a6c7929e7a39d87a
Vladyslav <rek0ra921@gmail.com>
2019-01-25 17:24:54 +0200

    detail_screen > Improve detail activity layout

--- app/src/main/java/com/example/vshcheglov/simpleweather/DetailActivity.kt
+++ app/src/main/java/com/example/vshcheglov/simpleweather/DetailActivity.kt
@@ -1,11 +1,8 @@
 package com.example.vshcheglov.simpleweather
 
-import android.graphics.Color
+import com.example.vshcheglov.simpleweather.R
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
-import android.support.v4.content.res.ResourcesCompat
-import android.widget.TextView
 import com.example.vshcheglov.simpleweather.domain.commands.RequestDayForecastCommand
 import com.example.vshcheglov.simpleweather.domain.model.Forecast
 import com.example.vshcheglov.simpleweather.extensions.toDateString
@@ -14,7 +11,7 @@ import kotlinx.android.synthetic.main.activity_detail_ativity.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
-class DetailAtivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     companion object {
         val ID = "DetailActivity:id"
@@ -24,6 +21,8 @@ class DetailAtivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_ativity)
+
+        supportActionBar?.elevation = 0.0f
 
         detailCity.text = intent.getStringExtra(CITY_NAME)
 
@@ -38,17 +37,7 @@ class DetailAtivity : AppCompatActivity() {
         Picasso.get().load(forecast.iconUrl).into(detailImage)
         detailDate.text = forecast.date.toDateString()
         detailDescription.text = forecast.description
-        bindTemperature(forecast.high, detailMaxTemperature)
-        bindTemperature(forecast.low, detailMinTemperature)
-    }
-
-    private fun bindTemperature(temperature: Int, textView: TextView) {
-        textView.text = temperature.toString()
-        val color = when{
-            temperature < 0 -> android.R.color.holo_blue_dark
-            temperature in 0..23 -> android.R.color.holo_green_dark
-            else -> android.R.color.holo_red_dark
-        }
-        textView.setTextColor(ContextCompat.getColor(this, color))
+        detailMaxTemperature.text = forecast.high.toString()
+        detailMinTemperature.text = forecast.low.toString()
     }
 }
